35cc7a4a1299383dc5853315aae6d44106a2d5c0
Md Khaled Hasan Manna <khmannaict13@gmail.com>
2018-07-25 22:44:12 +0600

    Initial Commit

---
+++ Protocal and DelegateTests/Protocal_and_DelegateTests.swift
@@ -0,0 +1,36 @@
+//
+//  Protocal_and_DelegateTests.swift
+//  Protocal and DelegateTests
+//
+//  Created by USER on 7/25/18.
+//  Copyright © 2018 mCubes. All rights reserved.
+//
+
+import XCTest
+@testable import Protocal_and_Delegate
+
+class Protocal_and_DelegateTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
